38f3bb690e0ba4b40e1445f39d294f4f37dee624
Swapnil8074 <swapnilshukla201@gmail.com>
2019-03-19 21:13:58 +0530

    added sequelize.sync() function so that it creates table if does`nt exist in database.

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
-const sequelize = require('./config/db');
-
+const path = require("path");
+const sequelize = require("./config/db");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // middlewares
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 
 // setting view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // testing database connection
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+// syncing sequelize so that it creates table as per model if not table does`nt exist..
+sequelize.sync();
 
 // importing routes
 const todoRoutes = require("./routes/todo");
 
-app.use('/todo', todoRoutes);
+app.use("/todo", todoRoutes);
 
-
-
-app.listen(3000, function () {
-    console.log(`Server listening on port 3000`);
-})
+app.listen(3000, function() {
+  console.log(`Server listening on port 3000`);
+});
